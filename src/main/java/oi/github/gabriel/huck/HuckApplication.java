9b0dee2cec4dcf05dd9b05d6ab022a34ed249f8c
Gabriel Fernandes <mendesgabrilfernandes@gmail.com>
2026-04-27 11:19:03 -0300

    feat:"classe man"

---
+++ src/main/java/oi/github/gabriel/huck/HuckApplication.java
@@ -0,0 +1,13 @@
+package oi.github.gabriel.huck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuckApplication.class, args);
+	}
+
+}
