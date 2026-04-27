3cfedf9af52483d53f7003df0844292a38adc8f0
Gabriel Fernandes <mendesgabrilfernandes@gmail.com>
2026-04-27 11:16:48 -0300

    feat:"criação repositorio"

---
+++ src/main/java/oi/github/gabriel/huck/repository/EquipamentoRepository.java
@@ -0,0 +1,7 @@
+package oi.github.gabriel.huck.repository;
+
+import oi.github.gabriel.huck.model.Equipamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipamentoRepository extends JpaRepository<Equipamento,Long> {
+}
